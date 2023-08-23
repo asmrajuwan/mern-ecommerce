0e3cd44a080fa4cc2e53666422068b7da211def4
asmrajuwan <rajuwan.rj@gmail.com>
2023-08-24 01:15:53 +0600

    create api

--- client/src/pages/Admin/CraeteCategory.js
+++ client/src/pages/Admin/CraeteCategory.js
@@ -1,3 +1,4 @@
+import { Modal } from 'antd';
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
@@ -7,31 +8,37 @@ import Layout from "../../components/Layout/Layout";
 
 const CraeteCategory = () => {
     const [categories, setCategories] = useState([]);
-    const [name,setName] = useState("")
+    const [name, setName] = useState("");
+    const [visible, setVisible] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const [updatedName, setUpdatedName] = useState("");
 
-    const handleSubmit = async (e)=>{
-      e.preventDefault()
-      try {
-        const {data} = await axios.post(`${process.env.REACT_APP_API}/api/v1/category/create-category`,{name})
-        if(data?.success){
-          toast.success(`${name} is created`)
-          getAllCategories();
-        } else{
-          toast.error(data.message)
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.post(
+                `${process.env.REACT_APP_API}/api/v1/category/create-category`,
+                { name }
+            );
+            if (data?.success) {
+                toast.success(`${name} is created`);
+                getAllCategories();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong in input form");
         }
-      } catch (error) {
-        console.log(error)
-        toast.error('Something went wrong in input form')
-      }
-    }
+    };
 
     const getAllCategories = async () => {
         try {
             const { data } = await axios.get(
                 `${process.env.REACT_APP_API}/api/v1/category/categories`
             );
-            if (data.success) {
-                setCategories(data.categories);
+            if (data?.success) {
+                setCategories(data?.categories);
             }
         } catch (error) {
             console.log(error);
@@ -42,6 +49,49 @@ const CraeteCategory = () => {
     useEffect(() => {
         getAllCategories();
     }, []);
+
+const handleUpdate =async (e)=>{
+    e.preventDefault()
+    try {
+        const {data} = await 
+        axios.put
+        (`${process.env.REACT_APP_API}/api/v1/category/update-category/${selected._id}`,
+        { name: updatedName });
+        if (data?.success) {
+            toast.success(`${updatedName} is updated`);
+            setSelected(null);
+            setUpdatedName("");
+            setVisible(false);
+            getAllCategories();
+          } else {
+            toast.error(data.message);
+          }
+    } catch (error) {
+        console.log(error)
+        toast.error('something went wrong')
+    }
+}
+const handleDelete =async (pId)=>{
+    
+    try {
+        const {data} = await 
+        axios.delete
+        (`${process.env.REACT_APP_API}/api/v1/category/delete-category/${pId}`)
+        if (data?.success) {
+            toast.success(`A category is deleted`);
+           
+            
+            
+            getAllCategories();
+          } else {
+            toast.error(data.message);
+          }
+    } catch (error) {
+        console.log(error)
+        toast.error('something went wrong')
+    }
+}
+
     return (
         <Layout>
             <div className="container-fluid m-3 p-3">
@@ -52,37 +102,54 @@ const CraeteCategory = () => {
                     <div className="w-3/4">
                         <h3 className="text-2xl font-bold">Manage Category</h3>
                         <div className="p-3 w-1/2">
-                          <CategoryForm  handleSubmit={handleSubmit} value={name} setValue={setName}/>
+                            <CategoryForm
+                                handleSubmit={handleSubmit}
+                                value={name}
+                                setValue={setName}
+                            />
                         </div>
                         <div className="w-3/4">
                             <table className="table">
                                 <thead>
                                     <tr>
-                                        
-                                        
                                         <th scope="col">Name</th>
                                         <th scope="col">Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                      {categories?.map(c => (
+                                    {categories?.map((c) => (
                                         <>
-                                    <tr>
-                                        <td key={c._id}>{c.name}</td>
-                                      <td><button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-                                        Edit
-                                      </button></td>
-                                    </tr>
+                                            <tr>
+                                                <td key={c._id}>{c.name}</td>
+                                                <td>
+                                                    <div>
+                                                        <button 
+                                                        className="bg-blue-500
+                                                         hover:bg-blue-600
+                                                          text-white font-bold 
+                                                          py-2 px-4 rounded ml-2"
+                                                          onClick={()=>{setVisible(true); setUpdatedName(c.name); setSelected(c)}}>
+                                                            Edit
+                                                        </button>
+                                                        <button onClick={()=> {handleDelete(c._id)}}
+                                                        className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded ml-2">
+                                                            Delete
+                                                        </button>
+                                                    </div>
+                                                </td>
+                                            </tr>
                                         </>
-                                      ))} 
-                                   
-                                    
+                                    ))}
                                 </tbody>
                             </table>
                         </div>
+                        <Modal 
+                        onCancel={() => setVisible(false)} 
+                        footer={null}
+                        visible ={visible}><CategoryForm value={updatedName} setValue={setUpdatedName} handleSubmit={handleUpdate} /></Modal>
                     </div>
                 </div>
-            </div>
+            </div>  
         </Layout>
     );
 };
--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,23 +1,71 @@
-import React from 'react';
-import AdminMenu from '../../components/Layout/AdminMenu';
-import Layout from '../../components/Layout/Layout';
+// import { Select } from "antd";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Select from "react-select";
+
+import { toast } from "react-hot-toast";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import Layout from "../../components/Layout/Layout";
+
+const { Option } = Select;
 
 const CreateProduct = () => {
-  return (
-    <Layout>
-       <div className="container-fluid m-3 p-3">
-  <div className="flex">
-    <div className="w-1/4">
-      <AdminMenu/>
-    </div>
-    <div className="w-3/4">
-      <h3 className="text-2xl font-bold">Create Product</h3>
-    </div>
-  </div>
-</div>
+    const [categories, setCategories] = useState([]);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [quantity, setQuantity] = useState("");
+    const [shipping, setShipping] = useState("");
+    const [photo, setPhoto] = useState("");
 
-    </Layout>
-  )
-}
+    const getAllCategories = async () => {
+        try {
+            const { data } = await axios.get(
+                `${process.env.REACT_APP_API}/api/v1/category/categories`
+            );
+            if (data?.success) {
+                setCategories(data?.categories);
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error("Something went wrong in getting categories");
+        }
+    };
+
+    useEffect(() => {
+        getAllCategories();
+    }, []);
+
+    return (
+        <Layout>
+            <div className="container-fluid m-3 p-3">
+                <div className="flex">
+                    <div className="w-1/4">
+                        <AdminMenu />
+                    </div>
+                    <div className="w-3/4">
+                        <h3 className="text-2xl font-bold">Create Product</h3>
+                        <div className="m-1 w-3/4">
+                            <Select
+                                options={categories.map((c) => ({
+                                    value: c._id,
+                                    label: c.name,
+                                }))}
+                                placeholder="Select a category"
+                                isSearchable
+                                className="bg-white  rounded-md shadow-sm text-gray-700 focus:ring focus:ring-opacity-50 focus:ring-blue-500 focus:border-blue-500 py-2 px-4 w-full mb-3"
+                                onChange={(selectedOption) => {
+                                    setCategory(selectedOption.value);
+                                }}
+                            />
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
 
 export default CreateProduct;
