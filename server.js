3f529544a8856317e4710769e61c8bb2973be172
asmrajuwan <rajuwan.rj@gmail.com>
2023-09-06 10:23:10 +0600

    server updated

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+import path from "path";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
@@ -19,13 +20,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 app.use("/api/v1/auth", authRoute)
 app.use("/api/v1/category", categoryRoutes)
 app.use("/api/v1/product", productRoutes)
 
-app.get('/', (req,res)=> {
-    res.send("<h1>Welcome to bazarshodai</h1>")
+app.use('*',function(req,res) {
+    res.sendFile(__dirname,'./client/build/index.html')
 });
 
 const port = process.env.PORT || 8001;  
